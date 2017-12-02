a40d264919f6f718b6d877bbeb5617a8d88d49a5
phc5 <paulhyunchong@gmail.com>
2017-12-02 10:39:39 -0800

    get current week's scores and display in table

--- index.js
+++ index.js
@@ -45,4 +45,38 @@ cfonts.say('nflmon', {
     maxLength: '0'
 });
 
-console.log(table.toString());
+const spinner = ora('Loading NFL games').start();
+const scoreJSON = 'https://www.nfl.com/liveupdate/scorestrip/ss.json';
+
+axios.get(scoreJSON)
+    .then(function (response) {
+        spinner.stop();
+        const data = response.data;
+        data.gms
+            .map(game => {
+                const homeTeam = `${game.h} ${game.hnn}`;
+                const awayTeam = `${game.v} ${game.vnn}`;
+                const homeScore = game.hs;
+                const awayScore = game.vs;
+                const time = `${game.d} ${game.t}`;
+                return [
+                    time,
+                    homeTeam,
+                    homeScore,
+                    awayTeam,
+                    awayScore
+                ];
+            })
+            .forEach(game => table.push(game));
+
+        if (table.length === 0) {
+            console.log('No data was received...Is the season over? :(');
+        } else {
+            console.log(`Week ${data.w} data of the ${data.y}  ${(data.t === 'REG' ? 'regular season' : 'playoffs')} from nfl.com at ${new Date().toLocaleTimeString()}`);
+            console.log(table.toString());
+        }
+    })
+    .catch(function (error) {
+        spinner.stop();
+        console.log('Oops... something went wrong: ' + error);
+    });
